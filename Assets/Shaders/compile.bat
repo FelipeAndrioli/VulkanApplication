92445eb303988f20141410b410d5a537d6a078d8
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-07-18 19:56:48 -0300

    Added particle shaders

--- Assets/Shaders/compile.bat
+++ Assets/Shaders/compile.bat
@@ -1,3 +1,6 @@
 C:/VulkanSDK/1.3.250.0/Bin/glslc.exe shader.vert -o vert.spv
 C:/VulkanSDK/1.3.250.0/Bin/glslc.exe shader.frag -o frag.spv
+C:/VulkanSDK/1.3.250.0/Bin/glslc.exe particle_shader.comp -o particle_shader_comp.spv
+C:/VulkanSDK/1.3.250.0/Bin/glslc.exe particle_shader.vert -o particle_shader_vert.spv
+C:/VulkanSDK/1.3.250.0/Bin/glslc.exe particle_shader.frag -o particle_shader_frag.spv
 pause
