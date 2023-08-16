05ff26998dfb01f8183ba98cb57afe8963446002
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-08-16 11:32:56 -0300

    Added Instance specific class

---
+++ src/Instance.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include "Vulkan.h"
+
+namespace Engine {
+	class Instance {
+	public:
+		Instance(const std::vector<const char*> &validationLayers, const bool &enableValidationLayers);
+		~Instance();
+
+		VkInstance& GetHandle();
+		const std::vector<const char*>& GetValidationLayers();
+	private:
+		const std::vector<const char*> m_ValidationLayers;
+		const bool m_EnableValidationLayers;
+		VkInstance m_VulkanInstance = VK_NULL_HANDLE;
+
+		std::vector<const char*> getRequiredExtensions();
+		bool checkValidationLayerSupport();
+		void checkRequiredExtensions(uint32_t glfwExtensionCount, const char** glfwExtensions,
+			std::vector<VkExtensionProperties> vulkanSupportedExtensions);
+	};
+}
