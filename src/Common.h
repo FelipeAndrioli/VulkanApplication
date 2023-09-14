5b0443374dadfb2e3468121274274a2d35459401
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-09-14 19:31:32 -0300

    Removing hardcoded vertices and indices

--- src/Common.h
+++ src/Common.h
@@ -66,17 +66,6 @@ namespace Engine {
 		}
 	};
 
-	// Temporary
-	inline std::vector<Assets::Vertex> vertices = {
-		{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-		{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-		{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
-	};
-
-	inline const std::vector<uint16_t> indices = {
-		0, 1, 2, 2, 3, 0
-	};
-	
 	struct UniformBufferObject {
 		alignas(16) glm::mat4 model;
 		alignas(16) glm::mat4 view;
