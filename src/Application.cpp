7bcdba1c371a1914b8b8e737c0529326be7d94d6
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-12-11 10:17:10 -0300

    Reestructuring Application

--- src/Application.cpp
+++ src/Application.cpp
@@ -10,69 +10,142 @@ namespace Engine {
 		m_Window.reset(new class Window(&m_WindowSettings));
 		m_Instance.reset(new class Instance(c_ValidationLayers, c_EnableValidationLayers));
 		m_DebugMessenger.reset(c_EnableValidationLayers ? new class DebugUtilsMessenger(m_Instance->GetHandle()) : nullptr);
+	
+		m_Window->Render = std::bind(&Application::Draw, this);
+		m_Window->OnKeyPress = std::bind(&Application::processKey, this, std::placeholders::_1, std::placeholders::_2,
+			std::placeholders::_3, std::placeholders::_4);
+		m_Window->OnResize = std::bind(&Application::processResize, this, std::placeholders::_1, std::placeholders::_2);
 	}
 
 	Application::~Application() {
 		Shutdown();
 	}
 
-	/* Disabled due to the new architecture, will think in a way to make it work later*/
-	void Application::drawRayTraced(VkCommandBuffer& p_CommandBuffer, uint32_t imageIndex) {
-		VkSubmitInfo submitInfo{};
-		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-		
-		vkWaitForFences(m_LogicalDevice->GetHandle(), 1, m_ComputeInFlightFences->GetHandle(m_CurrentFrame), VK_TRUE, UINT64_MAX);
-		updateComputeUniformBuffer(m_CurrentFrame);
-		vkResetFences(m_LogicalDevice->GetHandle(), 1, m_ComputeInFlightFences->GetHandle(m_CurrentFrame));
+	void Application::Init() {
+		InitVulkan();
 
-		auto computeCommandBuffer = m_ComputeCommandBuffers->Begin(m_CurrentFrame);
-		//recordComputeCommandBuffer(computeCommandBuffer);
-		m_ComputeCommandBuffers->End(m_CurrentFrame);
+		m_UI.reset(new class UI(m_Window->GetHandle(), m_Instance.get(), m_PhysicalDevice.get(), m_LogicalDevice.get(),
+			m_SwapChain.get(), g_MinImageCount));
+	}
 
-		submitInfo.commandBufferCount = 1;
-		submitInfo.pCommandBuffers = &m_ComputeCommandBuffers->GetCommandBuffer(m_CurrentFrame);
-		submitInfo.signalSemaphoreCount = 1;
-		submitInfo.pSignalSemaphores = m_ComputeFinishedSemaphores->GetHandle(m_CurrentFrame);
+	void Application::SetActiveScene(Assets::Scene* scene) {
 
-		if (vkQueueSubmit(m_LogicalDevice->GetComputeQueue(), 1, &submitInfo, *m_ComputeInFlightFences->GetHandle(m_CurrentFrame)) != VK_SUCCESS) {
-			throw std::runtime_error("Failed to submit compute command buffer!");
+		if (scene == nullptr) return;
+
+		p_ActiveScene = scene;
+		p_ActiveScene->OnCreate();
+
+		m_Window->Update = std::bind(&Application::Update, this, std::placeholders::_1);
+	}
+
+	void Application::Run() {
+		m_Window->Run();
+
+		m_LogicalDevice->WaitIdle();
+	}
+
+	void Application::Update(float t) {
+		if (p_ActiveScene) {
+			p_ActiveScene->OnUpdate(t);
 		}
+	}
 
-		VkRenderPassBeginInfo renderPassInfo{};
-		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-		//renderPassInfo.renderPass = m_TempRayTracerPipeline->GetRenderPass().GetHandle();
-		//renderPassInfo.framebuffer = m_SwapChain->GetSwapChainFramebuffer(imageIndex);
-		renderPassInfo.renderArea.offset = { 0, 0 };
-		renderPassInfo.renderArea.extent = m_SwapChain->GetSwapChainExtent();
+	void Application::InitVulkan() {
+		m_Surface.reset(new class Surface(m_Instance->GetHandle(), *m_Window->GetHandle()));
+		m_PhysicalDevice.reset(new class PhysicalDevice(m_Instance->GetHandle(), m_Surface->GetHandle()));
+		m_LogicalDevice.reset(new class LogicalDevice(m_Instance.get(), m_PhysicalDevice.get()));
+		m_SwapChain.reset(new class SwapChain(m_PhysicalDevice.get(), m_Window.get(), m_LogicalDevice.get(), m_Surface->GetHandle()));
+		m_DepthBuffer.reset(new class DepthBuffer(&m_PhysicalDevice->GetHandle(), &m_LogicalDevice->GetHandle(), m_SwapChain.get()));
 
-		VkClearValue clearColor = { {{0.0f, 0.0f, 0.0f, 1.0f}} };
-		renderPassInfo.clearValueCount = 1;
-		renderPassInfo.pClearValues = &clearColor;
+		m_DefaultRenderPass.reset(new class RenderPass(m_SwapChain.get(), m_LogicalDevice->GetHandle(), m_DepthBuffer.get()));
+		CreateFramebuffers(m_DefaultRenderPass->GetHandle());
 
-		vkCmdBeginRenderPass(p_CommandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
-		vkCmdBindPipeline(p_CommandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_TempRayTracerPipeline->GetHandle());
+		m_CommandPool.reset(new class CommandPool(m_LogicalDevice->GetHandle(), m_PhysicalDevice->GetQueueFamilyIndices()));	
+		m_CommandBuffers.reset(new class CommandBuffer(MAX_FRAMES_IN_FLIGHT, m_CommandPool->GetHandle(), 
+			m_LogicalDevice->GetHandle()));
 
-		VkViewport viewport = {};
-		viewport.x = 0.0f;
-		viewport.y = 0.0f;
-		viewport.width = (float)m_SwapChain->GetSwapChainExtent().width;
-		viewport.height = (float)m_SwapChain->GetSwapChainExtent().height;
-		viewport.minDepth = 0.0f;
-		viewport.maxDepth = 1.0f;
+		/*
+		m_ComputeCommandBuffers.reset(new class CommandBuffer(MAX_FRAMES_IN_FLIGHT, m_CommandPool->GetHandle(), 
+			m_LogicalDevice->GetHandle()));
+		*/
+		
+		createSyncObjects();
+	
+		p_ActiveScene->SetupScene(*m_LogicalDevice, *m_PhysicalDevice, *m_CommandPool, *m_SwapChain, *m_DepthBuffer, 
+			m_DefaultRenderPass->GetHandle());
+	}
 
-		vkCmdSetViewport(p_CommandBuffer, 0, 1, &viewport);
+	void Application::Shutdown() {
+		m_UI.reset();
 
-		VkRect2D scissor = {};
-		scissor.offset = { 0, 0 };
-		scissor.extent = m_SwapChain->GetSwapChainExtent();
+		m_SwapChain.reset();
 
-		vkCmdSetScissor(p_CommandBuffer, 0, 1, &scissor);
+		m_ComputeUniformBuffers.reset();
+		//m_ComputePipeline.reset();
 
-		VkDeviceSize offsets[] = { 0 };
+		m_DepthBuffer.reset();
 
-		vkCmdBindVertexBuffers(p_CommandBuffer, 0, 1, &m_ShaderStorageBuffers->GetBuffer(m_CurrentFrame), offsets);
-		vkCmdDraw(p_CommandBuffer, PARTICLE_COUNT, 1, 0, 0);
-		vkCmdEndRenderPass(p_CommandBuffer);
+		ClearFramebuffers();
+
+		m_DefaultRenderPass.reset();
+
+		m_CommandBuffers.reset();
+		m_ComputeCommandBuffers.reset();
+
+		m_GraphicsPipeline.reset();
+		m_TempRayTracerPipeline.reset();
+
+		m_IndexBuffer.reset();
+		m_ShaderStorageBuffers.reset();
+		m_VertexBuffers.reset();
+
+		m_DebugMessenger.reset();
+		m_InFlightFences.reset();
+		m_ComputeInFlightFences.reset();
+
+		m_ImageAvailableSemaphores.reset();
+		m_RenderFinishedSemaphores.reset();
+		m_ComputeFinishedSemaphores.reset();
+
+		m_CommandPool.reset();
+
+		m_LogicalDevice.reset();
+		m_Surface.reset();
+		m_Instance.reset();
+		
+		glfwTerminate();
+	}
+
+	void Application::createSyncObjects() {
+		m_ImageAvailableSemaphores.reset(new class Semaphore(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
+		m_RenderFinishedSemaphores.reset(new class Semaphore(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
+		m_ComputeFinishedSemaphores.reset(new class Semaphore(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
+
+		m_InFlightFences.reset(new class Fence(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
+		m_ComputeInFlightFences.reset(new class Fence(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
+
+	}
+
+	void Application::recreateSwapChain() {
+		m_SwapChain->ReCreate();
+		m_DepthBuffer->Resize(m_SwapChain.get());
+
+		m_DefaultRenderPass.reset(new class RenderPass(m_SwapChain.get(), m_LogicalDevice->GetHandle(), m_DepthBuffer.get()));
+
+		ClearFramebuffers();
+		CreateFramebuffers(m_DefaultRenderPass->GetHandle());
+
+		m_CommandBuffers.reset(new class CommandBuffer(MAX_FRAMES_IN_FLIGHT, m_CommandPool->GetHandle(),
+			m_LogicalDevice->GetHandle()));
+		m_UI->Resize(m_SwapChain.get());
+	}
+
+	// temporary
+	void Application::updateComputeUniformBuffer(uint32_t currentImage) {
+		ComputeUniformBufferObject cubo{};
+		cubo.deltaTime = m_Window->GetLastFrameTime() * 0.1f;
+
+		//memcpy(m_ComputeUniformBuffers->GetBufferMemoryMapped(currentImage), &cubo, sizeof(cubo));
 	}
 
 	void Application::Draw() {
@@ -86,8 +159,6 @@ namespace Engine {
 	}
 
 	VkCommandBuffer* Application::BeginFrame() {
-		// move this function to DrawFrame function
-		m_UI->Draw(m_UserSettings, m_WindowSettings, p_ActiveScene);
 
 		VkResult result;
 
@@ -179,12 +250,12 @@ namespace Engine {
 		}
 		
 		vkCmdEndRenderPass(commandBuffer);
-
 	}
 
 	void Application::EndFrame(const VkCommandBuffer& commandBuffer) {
 		m_CommandBuffers->End(m_CurrentFrame);
 
+		m_UI->Draw(m_UserSettings, m_WindowSettings, p_ActiveScene);
 		m_UI->RecordCommands(m_CurrentFrame, m_ImageIndex);
 
 		VkPipelineStageFlags waitStages[] = { VK_PIPELINE_STAGE_VERTEX_INPUT_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
@@ -237,38 +308,63 @@ namespace Engine {
 		m_CurrentFrame = (m_CurrentFrame + 1) % MAX_FRAMES_IN_FLIGHT;
 	}
 
-	void Application::SetActiveScene(Assets::Scene* scene) {
+	/* Disabled due to the new architecture, will think in a way to make it work later*/
+	void Application::drawRayTraced(VkCommandBuffer& p_CommandBuffer, uint32_t imageIndex) {
+		VkSubmitInfo submitInfo{};
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+		
+		vkWaitForFences(m_LogicalDevice->GetHandle(), 1, m_ComputeInFlightFences->GetHandle(m_CurrentFrame), VK_TRUE, UINT64_MAX);
+		updateComputeUniformBuffer(m_CurrentFrame);
+		vkResetFences(m_LogicalDevice->GetHandle(), 1, m_ComputeInFlightFences->GetHandle(m_CurrentFrame));
 
-		if (scene == nullptr) return;
+		auto computeCommandBuffer = m_ComputeCommandBuffers->Begin(m_CurrentFrame);
+		//recordComputeCommandBuffer(computeCommandBuffer);
+		m_ComputeCommandBuffers->End(m_CurrentFrame);
 
-		p_ActiveScene = scene;
-		p_ActiveScene->OnCreate();
+		submitInfo.commandBufferCount = 1;
+		submitInfo.pCommandBuffers = &m_ComputeCommandBuffers->GetCommandBuffer(m_CurrentFrame);
+		submitInfo.signalSemaphoreCount = 1;
+		submitInfo.pSignalSemaphores = m_ComputeFinishedSemaphores->GetHandle(m_CurrentFrame);
 
-		m_Window->Update = std::bind(&Application::Update, this, std::placeholders::_1);
-	}
-
-	void Application::Run() {
-
-		m_Window->Render = std::bind(&Application::Draw, this);
-		m_Window->OnKeyPress = std::bind(&Application::processKey, this, std::placeholders::_1, std::placeholders::_2,
-			std::placeholders::_3, std::placeholders::_4);
-		m_Window->OnResize = std::bind(&Application::processResize, this, std::placeholders::_1, std::placeholders::_2);
-		m_Window->Run();
-
-		m_LogicalDevice->WaitIdle();
-	}
-
-	void Application::Update(float t) {
-		if (p_ActiveScene) {
-			p_ActiveScene->OnUpdate(t);
+		if (vkQueueSubmit(m_LogicalDevice->GetComputeQueue(), 1, &submitInfo, *m_ComputeInFlightFences->GetHandle(m_CurrentFrame)) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to submit compute command buffer!");
 		}
-	}
 
-	void Application::Init() {
-		InitVulkan();
+		VkRenderPassBeginInfo renderPassInfo{};
+		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+		//renderPassInfo.renderPass = m_TempRayTracerPipeline->GetRenderPass().GetHandle();
+		//renderPassInfo.framebuffer = m_SwapChain->GetSwapChainFramebuffer(imageIndex);
+		renderPassInfo.renderArea.offset = { 0, 0 };
+		renderPassInfo.renderArea.extent = m_SwapChain->GetSwapChainExtent();
 
-		m_UI.reset(new class UI(m_Window->GetHandle(), m_Instance.get(), m_PhysicalDevice.get(), m_LogicalDevice.get(),
-			m_SwapChain.get(), g_MinImageCount));
+		VkClearValue clearColor = { {{0.0f, 0.0f, 0.0f, 1.0f}} };
+		renderPassInfo.clearValueCount = 1;
+		renderPassInfo.pClearValues = &clearColor;
+
+		vkCmdBeginRenderPass(p_CommandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+		vkCmdBindPipeline(p_CommandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_TempRayTracerPipeline->GetHandle());
+
+		VkViewport viewport = {};
+		viewport.x = 0.0f;
+		viewport.y = 0.0f;
+		viewport.width = (float)m_SwapChain->GetSwapChainExtent().width;
+		viewport.height = (float)m_SwapChain->GetSwapChainExtent().height;
+		viewport.minDepth = 0.0f;
+		viewport.maxDepth = 1.0f;
+
+		vkCmdSetViewport(p_CommandBuffer, 0, 1, &viewport);
+
+		VkRect2D scissor = {};
+		scissor.offset = { 0, 0 };
+		scissor.extent = m_SwapChain->GetSwapChainExtent();
+
+		vkCmdSetScissor(p_CommandBuffer, 0, 1, &scissor);
+
+		VkDeviceSize offsets[] = { 0 };
+
+		vkCmdBindVertexBuffers(p_CommandBuffer, 0, 1, &m_ShaderStorageBuffers->GetBuffer(m_CurrentFrame), offsets);
+		vkCmdDraw(p_CommandBuffer, PARTICLE_COUNT, 1, 0, 0);
+		vkCmdEndRenderPass(p_CommandBuffer);
 	}
 
 	void Application::processKey(int key, int scancode, int action, int mods) {
@@ -315,31 +411,6 @@ namespace Engine {
 		m_Framebuffers.clear();
 	}
 
-	void Application::InitVulkan() {
-		m_Surface.reset(new class Surface(m_Instance->GetHandle(), *m_Window->GetHandle()));
-		m_PhysicalDevice.reset(new class PhysicalDevice(m_Instance->GetHandle(), m_Surface->GetHandle()));
-		m_LogicalDevice.reset(new class LogicalDevice(m_Instance.get(), m_PhysicalDevice.get()));
-		m_SwapChain.reset(new class SwapChain(m_PhysicalDevice.get(), m_Window.get(), m_LogicalDevice.get(), m_Surface->GetHandle()));
-		m_DepthBuffer.reset(new class DepthBuffer(&m_PhysicalDevice->GetHandle(), &m_LogicalDevice->GetHandle(), m_SwapChain.get()));
-
-		m_DefaultRenderPass.reset(new class RenderPass(m_SwapChain.get(), m_LogicalDevice->GetHandle(), m_DepthBuffer.get()));
-		CreateFramebuffers(m_DefaultRenderPass->GetHandle());
-
-		m_CommandPool.reset(new class CommandPool(m_LogicalDevice->GetHandle(), m_PhysicalDevice->GetQueueFamilyIndices()));	
-		m_CommandBuffers.reset(new class CommandBuffer(MAX_FRAMES_IN_FLIGHT, m_CommandPool->GetHandle(), 
-			m_LogicalDevice->GetHandle()));
-
-		/*
-		m_ComputeCommandBuffers.reset(new class CommandBuffer(MAX_FRAMES_IN_FLIGHT, m_CommandPool->GetHandle(), 
-			m_LogicalDevice->GetHandle()));
-		*/
-		
-		createSyncObjects();
-	
-		p_ActiveScene->SetupScene(*m_LogicalDevice, *m_PhysicalDevice, *m_CommandPool, *m_SwapChain, *m_DepthBuffer, 
-			m_DefaultRenderPass->GetHandle());
-	}
-
 	/* TODO: not ready AON
 	void Application::recordComputeCommandBuffer(VkCommandBuffer commandBuffer) {
 		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, m_ComputePipeline->GetHandle());
@@ -349,77 +420,4 @@ namespace Engine {
 		vkCmdDispatch(commandBuffer, PARTICLE_COUNT / 256, 1, 1);
 	}
 	*/
-
-	void Application::createSyncObjects() {
-		m_ImageAvailableSemaphores.reset(new class Semaphore(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
-		m_RenderFinishedSemaphores.reset(new class Semaphore(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
-		m_ComputeFinishedSemaphores.reset(new class Semaphore(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
-
-		m_InFlightFences.reset(new class Fence(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
-		m_ComputeInFlightFences.reset(new class Fence(m_LogicalDevice->GetHandle(), MAX_FRAMES_IN_FLIGHT));
-
-	}
-
-	void Application::recreateSwapChain() {
-		m_SwapChain->ReCreate();
-		m_DepthBuffer->Resize(m_SwapChain.get());
-
-		m_DefaultRenderPass.reset(new class RenderPass(m_SwapChain.get(), m_LogicalDevice->GetHandle(), m_DepthBuffer.get()));
-
-		ClearFramebuffers();
-		CreateFramebuffers(m_DefaultRenderPass->GetHandle());
-
-		m_CommandBuffers.reset(new class CommandBuffer(MAX_FRAMES_IN_FLIGHT, m_CommandPool->GetHandle(),
-			m_LogicalDevice->GetHandle()));
-		m_UI->Resize(m_SwapChain.get());
-	}
-
-	// temporary
-	void Application::updateComputeUniformBuffer(uint32_t currentImage) {
-		ComputeUniformBufferObject cubo{};
-		cubo.deltaTime = m_Window->GetLastFrameTime() * 0.1f;
-
-		//memcpy(m_ComputeUniformBuffers->GetBufferMemoryMapped(currentImage), &cubo, sizeof(cubo));
-	}
-
-	void Application::Shutdown() {
-		m_UI.reset();
-
-		m_SwapChain.reset();
-
-		m_ComputeUniformBuffers.reset();
-		//m_ComputePipeline.reset();
-
-		m_DepthBuffer.reset();
-
-		ClearFramebuffers();
-
-		m_DefaultRenderPass.reset();
-
-		m_CommandBuffers.reset();
-		m_ComputeCommandBuffers.reset();
-
-		m_GraphicsPipeline.reset();
-		m_TempRayTracerPipeline.reset();
-
-		m_IndexBuffer.reset();
-		m_ShaderStorageBuffers.reset();
-		m_VertexBuffers.reset();
-
-		m_DebugMessenger.reset();
-		m_InFlightFences.reset();
-		m_ComputeInFlightFences.reset();
-
-		m_ImageAvailableSemaphores.reset();
-		m_RenderFinishedSemaphores.reset();
-		m_ComputeFinishedSemaphores.reset();
-
-		m_CommandPool.reset();
-
-		m_LogicalDevice.reset();
-		m_Surface.reset();
-		m_Instance.reset();
-		
-		glfwTerminate();
-	}
 }
