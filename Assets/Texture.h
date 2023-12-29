7dff47e7c64d5a868bec62280d5c7da27124a845
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-12-29 14:43:03 -0300

    Added Texture struct

---
+++ Assets/Texture.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+namespace Engine {
+	class LogicalDevice;
+	class PhysicalDevice;
+	class CommandPool;
+	class Image;
+}
+
+namespace Assets {
+	struct Texture {
+		enum TextureType {
+			AMBIENT				= 0,
+			DIFFUSE				= 1,
+			SPECULAR			= 2,
+			SPECULAR_HIGHTLIGHT = 3,
+			BUMP				= 4,
+			DISPLACEMENT		= 5,
+			ALPHA				= 6,
+			REFLECTION			= 7,
+			ROUGHNESS			= 8,
+			METALLIC			= 9,
+			SHEEN				= 10,
+			EMISSIVE			= 11,
+			NORMAL				= 12
+		};
+
+		std::string Path = "";
+		TextureType Type;
+
+		std::unique_ptr<class Engine::Image> TextureImage;
+	};
+}
