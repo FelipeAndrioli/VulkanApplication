33fd3f0f8bab1122345df33cfcf699ecd948454d
Felipe Andrioli <felipeandrioli56@hotmail.com>
2024-01-23 20:13:01 -0300

    Updating API version and application name

--- src/Instance.cpp
+++ src/Instance.cpp
@@ -11,11 +11,11 @@ namespace Engine {
 
 		VkApplicationInfo appInfo{};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-		appInfo.pApplicationName = "RayTracer.exe";
+		appInfo.pApplicationName = "VulkanApplication.exe";
 		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.pEngineName = "No Engine";
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.apiVersion = VK_API_VERSION_1_0;
+		appInfo.apiVersion = VK_API_VERSION_1_1;
 
 		VkInstanceCreateInfo createInfo{};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
