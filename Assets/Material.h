feafe26eed60b2f493a021f39b4a371134c18a34
Felipe Andrioli <felipeandrioli56@hotmail.com>
2024-02-27 16:42:08 -0300

    Clean up; Changing from class to struct

--- Assets/Material.h
+++ Assets/Material.h
@@ -8,55 +8,39 @@
 
 #include "./Texture.h"
 
-namespace Engine {
-	class Buffer;
-	class DescriptorSets;
-}
-
 namespace Assets {
 	struct Texture;
 	struct TextureIndices;
 	enum TextureType;
 
-	class Material {
-	public:
+	struct MaterialProperties {
+		std::string Name = "";
 
-		Material();
-		~Material();
+		glm::vec3 Ambient = glm::vec3(1.0f);
+		glm::vec3 Diffuse = glm::vec3(1.0f);
+		glm::vec3 Specular = glm::vec3(1.0f);
+		glm::vec3 Transmittance = glm::vec3(1.0f);
+		glm::vec3 Emission = glm::vec3(1.0f);
 
-		struct MaterialProperties {
-			std::string Name = "";
+		float Shininess = 0.0f;
+		float Ior = 0.0f;
+		float Dissolve = 0.0f;
+		float Roughness = 0.0f;
+		float Metallic = 0.0f;
+		float Sheen = 0.0f;
+		float ClearcoatThickness = 0.0f;
+		float ClearcoatRoughness = 0.0f;
+		float Anisotropy = 0.0f;
+		float AnisotropyRotation = 0.0f;
+		//float Pad0 = 0.0f;
+		
+		int Pad2 = 0;
+		int Illum = 0;
+	};
 
-			glm::vec3 Ambient = glm::vec3(1.0f);
-			glm::vec3 Diffuse = glm::vec3(1.0f);
-			glm::vec3 Specular = glm::vec3(1.0f);
-			glm::vec3 Transmittance = glm::vec3(1.0f);
-			glm::vec3 Emission = glm::vec3(1.0f);
-
-			float Shininess = 0.0f;
-			float Ior = 0.0f;
-			float Dissolve = 0.0f;
-			float Roughness = 0.0f;
-			float Metallic = 0.0f;
-			float Sheen = 0.0f;
-			float ClearcoatThickness = 0.0f;
-			float ClearcoatRoughness = 0.0f;
-			float Anisotropy = 0.0f;
-			float AnisotropyRotation = 0.0f;
-			//float Pad0 = 0.0f;
-			
-			int Pad2 = 0;
-			int Illum = 0;
-		};
-
-	public:
+	struct Material {
 		MaterialProperties Properties = {};
-
 		size_t Index = 0;
-
 		Assets::TextureIndices MaterialTextureIndices;
-		//std::map<Assets::TextureType, Assets::Texture*> Textures;
-		//std::vector<uint32_t> Textures;
-		std::unique_ptr<class Engine::DescriptorSets> DescriptorSets;
 	};
 }
