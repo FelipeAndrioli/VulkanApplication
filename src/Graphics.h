1615eff26774c40841333938f529b5e1138db97d
Felipe Andrioli <felipeandrioli56@hotmail.com>
2024-07-03 13:54:26 -0300

    Added Image and Buffer descriptions

--- src/Graphics.h
+++ src/Graphics.h
@@ -20,19 +20,11 @@ namespace Engine::Graphics {
 		int bottom = 0;
 	};
 
-	struct GPUImage {
-		VkImage Image = VK_NULL_HANDLE;
-		VkImageView ImageView = VK_NULL_HANDLE;
-		VkSampler ImageSampler = VK_NULL_HANDLE;
-		VkImageLayout ImageLayout;
-
-		uint32_t Width;
-		uint32_t Height;
-		uint32_t MipLevels;
+	struct ImageDescription {
+		uint32_t Width = 0;
+		uint32_t Height = 0;
+		uint32_t MipLevels = 1;
 		uint32_t LayerCount = 1;
-
-		VkDeviceMemory Memory;
-		void* MemoryMapped;
 
 		VkFormat Format = VK_FORMAT_R8G8B8A8_SRGB;
 		VkImageTiling Tiling = VK_IMAGE_TILING_OPTIMAL;
@@ -40,27 +32,64 @@ namespace Engine::Graphics {
 		VkMemoryPropertyFlagBits MemoryProperty = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
 		VkImageAspectFlags AspectFlags = VK_IMAGE_ASPECT_COLOR_BIT;
 		VkImageViewType ViewType = VK_IMAGE_VIEW_TYPE_2D;
-		VkSampleCountFlagBits MsaaSamples;
+		VkSampleCountFlagBits MsaaSamples = VK_SAMPLE_COUNT_1_BIT;
 		VkImageType ImageType = VK_IMAGE_TYPE_2D;
+		VkSamplerAddressMode AddressMode = VK_SAMPLER_ADDRESS_MODE_REPEAT;
 	};
 
-	struct GPUBuffer {
+	struct GPUImage {
+		VkImage Image = VK_NULL_HANDLE;
+		VkImageView ImageView = VK_NULL_HANDLE;
+		VkSampler ImageSampler = VK_NULL_HANDLE;
+		VkImageLayout ImageLayout;
+
+		VkDeviceMemory Memory;
+		void* MemoryMapped;
+
+		ImageDescription Description = {};
+	};
+
+	struct Texture : public GPUImage {
+
+		enum TextureType {
+			AMBIENT				= 0,
+			DIFFUSE				= 1,
+			SPECULAR			= 2,
+			SPECULAR_HIGHTLIGHT = 3,
+			BUMP				= 4,
+			DISPLACEMENT		= 5,
+			ALPHA				= 6,
+			REFLECTION			= 7,
+			ROUGHNESS			= 8,
+			METALLIC			= 9,
+			SHEEN				= 10,
+			EMISSIVE			= 11,
+			NORMAL				= 12,
+			UNKNOWN				= 13
+		} Type = TextureType::UNKNOWN;
+
+		std::string Name = "";
+	};
+
+	struct BufferDescription {
+		VkBufferUsageFlags Usage;
+		VkMemoryPropertyFlagBits MemoryProperty = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+		size_t BufferSize = 0;
 
 		struct BufferChunk {
 			size_t DataSize;
 			size_t ChunkSize;
 		};
 
+		std::vector<BufferChunk> Chunks;
+	};
+
+	struct GPUBuffer {
 		VkBuffer Handle = VK_NULL_HANDLE;
-		
-		VkBufferUsageFlags Usage;
 
-		size_t BufferSize = 0;
-
-		VkMemoryPropertyFlagBits MemoryProperty = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
 		VkDeviceMemory Memory;
 		void* MemoryMapped;
 
-		std::vector<BufferChunk> Chunks;
+		BufferDescription Description = {};
 	};
 }
