fa18a4ee27562ed438a9d6546099dea408939dfb
Felipe Andrioli <felipeandrioli56@hotmail.com>
2024-10-18 06:28:59 -0300

    Updating shaders

--- src/Assets/Shaders/compile.bat
+++ src/Assets/Shaders/compile.bat
@@ -12,4 +12,6 @@ C:/VulkanSDK/1.3.250.0/Bin/glslc.exe skybox.frag -o skybox_frag.spv
 
 C:/VulkanSDK/1.3.250.0/Bin/glslc.exe outline.vert -o outline_vert.spv
 C:/VulkanSDK/1.3.250.0/Bin/glslc.exe outline.frag -o outline_frag.spv
+
+C:/VulkanSDK/1.3.250.0/Bin/glslc.exe transparent_ps.frag -o transparent_frag.spv
 pause
