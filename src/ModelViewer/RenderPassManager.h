b7ae5e02cb9798aa157fc6ceafbf5053cb064e41
Felipe Andrioli <felipeandrioli56@hotmail.com>
2025-01-07 17:19:36 -0300

    Removing not used render pass

--- src/ModelViewer/RenderPassManager.h
+++ src/ModelViewer/RenderPassManager.h
@@ -7,7 +7,6 @@
 namespace Graphics {
 	extern RenderPass g_ColorRenderPass;
 	extern RenderPass g_PostEffectsRenderPass;
-	extern RenderPass g_DebugRenderPass;
 
 	void InitializeStaticRenderPasses(uint32_t width, uint32_t height);
 	void ResizeRenderPasses(uint32_t width, uint32_t height);
