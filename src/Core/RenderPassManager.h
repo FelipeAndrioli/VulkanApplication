bbff00f44845cc038f5191d8ff25fd5b3d546e26
Felipe Andrioli <felipeandrioli56@hotmail.com>
2025-01-14 06:15:52 -0300

    Added Render Pass Manager back to core

---
+++ src/Core/RenderPassManager.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../Core/VulkanHeader.h"
+#include "../Core/Graphics.h"
+#include "../Core/GraphicsDevice.h"
+
+namespace Graphics {
+	extern RenderPass g_ColorRenderPass;
+	extern RenderPass g_PostEffectsRenderPass;
+
+	void InitializeStaticRenderPasses(uint32_t width, uint32_t height);
+	void ResizeRenderPasses(uint32_t width, uint32_t height);
+	void ResizeRenderPass(RenderPass& renderPass, uint32_t width, uint32_t height);
+	void ShutdownRenderPasses();
+}
