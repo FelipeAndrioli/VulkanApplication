8b05ce1927b4eb7da5013d3b46108456855d92a4
Felipe Andrioli <felipeandrioli56@hotmail.com>
2025-02-14 05:08:34 -0300

    Initial draft

---
+++ src/Samples/Instancing/Instancing.cpp
@@ -0,0 +1,185 @@
+#include <memory>
+
+#include "../Core/Application.h"
+#include "../Core/GraphicsDevice.h"
+#include "../Core/Settings.h"
+#include "../Core/RenderTarget.h"
+#include "../Core/ResourceManager.h"
+
+#include "../Assets/Camera.h"
+#include "../Assets/Model.h"
+
+#include "../Utils/ModelLoader.h"
+
+class Instancing : public Application::IScene {
+public:
+	Instancing() {
+		settings.Title			= "Instancing.exe";
+		settings.Width			= 1600;
+		settings.Height			= 800;
+		settings.uiEnabled		= true;
+
+		m_Width					= settings.Width;
+		m_Height				= settings.Height;
+	}
+
+	virtual void StartUp()																		override;
+	virtual void CleanUp()																		override;
+	virtual void Update(const float constantT, const float deltaT, InputSystem::Input& input)	override;
+	virtual void RenderScene(const uint32_t currentFrame, const VkCommandBuffer& commandBuffer) override;
+	virtual void RenderUI()																		override;
+	virtual void Resize(uint32_t width, uint32_t height)										override;
+private:
+
+	struct PushConstant {
+		int materialIndex;
+	} m_PushConstant;
+
+	struct SceneGPUData {
+		float time				= 0.0f;				// 4
+		int extra_1				= 0;				// 8
+		int extra_2				= 0;				// 12
+		int extra_3				= 0;				// 16
+		glm::vec4 extra[7]		= {};				// 128
+		glm::mat4 view			= glm::mat4(1.0f);	// 192
+		glm::mat4 proj			= glm::mat4(1.0f);	// 256
+	} m_SceneGPUData;
+
+	struct ModelGPUData {
+		int flip_uv_vertically	= 0;				// 4
+		int extra_1				= 0;				// 8
+		int extra_2				= 0;				// 12
+		int extra_3				= 0;				// 16
+		glm::vec4 extra[7]		= {};				// 128
+		glm::mat4 model			= glm::mat4(1.0f);	// 192
+		glm::mat4 normal		= glm::mat4(1.0f);	// 256
+	} m_ModelGPUData;
+
+	std::shared_ptr<Assets::Model>	m_Model;
+
+	uint32_t						m_Width				= 0;
+	uint32_t						m_Height			= 0;
+
+	Assets::Camera					m_Camera			= {};
+
+	Graphics::Buffer				m_SceneDataBuffer	= {};
+	Graphics::Buffer				m_ModelDataBuffer	= {};
+	Graphics::Shader				m_VertexShader		= {};
+	Graphics::Shader				m_FragmentShader	= {};
+	Graphics::PipelineState			m_PSO				= {};
+	Graphics::InputLayout			m_PSOInputLayout	= {};
+
+	VkDescriptorSet					m_Set				= VK_NULL_HANDLE;
+};
+
+void Instancing::StartUp() {
+	m_Model									= ModelLoader::LoadModel("C:/Users/Felipe/Documents/current_projects/models/actual_models/stanford_dragon_sss_test/scene.gltf");
+	m_Model->Transformations.scaleHandler	= 11.2f;
+	m_Model->Transformations.translation	= glm::vec3(0.0f, 0.0f, -0.3f);
+	m_Model->Transformations.rotation		= glm::vec3(0.0f, -14.7, 0.0f);
+
+
+	m_Camera.Init(glm::vec3(0.0f, 0.0f, 5.0f), 45.0f, 270.0f, 0.0f, m_Width, m_Height);
+	
+	ResourceManager* rm = ResourceManager::Get();
+
+	m_PSOInputLayout = {
+		.pushConstants = {
+			{ VK_SHADER_STAGE_ALL_GRAPHICS, 0, sizeof(PushConstant) }
+		},
+		.bindings = {
+			{ 0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_VERTEX_BIT },															// Scene GPU Data
+			{ 1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_VERTEX_BIT },															// Model GPU Data
+			{ 2, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_FRAGMENT_BIT},															// Material Data
+			{ 3, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, static_cast<uint32_t>(rm->GetTextures().size()), VK_SHADER_STAGE_FRAGMENT_BIT }		// Textures Array 
+		}
+	};
+	
+	Graphics::GraphicsDevice* gfxDevice = Graphics::GetDevice();
+
+#ifdef RUNTIME_SHADER_COMPILATION
+	gfxDevice->LoadShader(VK_SHADER_STAGE_VERTEX_BIT,	m_VertexShader,		"../src/Samples/Instancing/vertex.glsl");
+	gfxDevice->LoadShader(VK_SHADER_STAGE_FRAGMENT_BIT, m_FragmentShader,	"../src/Samples/Instancing/fragment.glsl");
+#else
+	gfxDevice->LoadShader(VK_SHADER_STAGE_VERTEX_BIT,	m_VertexShader,		"vertex.spv");
+	gfxDevice->LoadShader(VK_SHADER_STAGE_FRAGMENT_BIT, m_FragmentShader,	"fragment.spv");
+#endif
+
+	m_SceneDataBuffer = gfxDevice->CreateBuffer(sizeof(SceneGPUData));
+	m_ModelDataBuffer = gfxDevice->CreateBuffer(sizeof(ModelGPUData));
+
+	Graphics::PipelineStateDescription desc = {};
+	desc.Name								= "Color Pipeline";
+	desc.vertexShader						= &m_VertexShader;
+	desc.fragmentShader						= &m_FragmentShader;
+	desc.cullMode							= VK_CULL_MODE_BACK_BIT;
+	desc.psoInputLayout						.push_back(m_PSOInputLayout);
+
+	gfxDevice->CreatePipelineState(desc, m_PSO, *gfxDevice->GetSwapChain().RenderTarget.get());
+
+	gfxDevice->CreateDescriptorSet(m_PSO.descriptorSetLayout, m_Set);
+	gfxDevice->WriteDescriptor(m_PSOInputLayout.bindings[0], m_Set, m_SceneDataBuffer);
+	gfxDevice->WriteDescriptor(m_PSOInputLayout.bindings[1], m_Set, m_ModelDataBuffer);
+	gfxDevice->WriteDescriptor(m_PSOInputLayout.bindings[2], m_Set, rm->GetMaterialBuffer());
+	gfxDevice->WriteDescriptor(m_PSOInputLayout.bindings[3], m_Set, rm->GetTextures());
+}
+
+void Instancing::CleanUp() {
+	Graphics::GraphicsDevice* gfxDevice = Graphics::GetDevice();
+
+	gfxDevice->DestroyShader(m_VertexShader);
+	gfxDevice->DestroyShader(m_FragmentShader);
+	gfxDevice->DestroyPipeline(m_PSO);
+
+	m_Model.reset();
+}
+
+void Instancing::Update(const float constantT, const float deltaT, InputSystem::Input& input) {
+	Graphics::GraphicsDevice* gfxDevice = Graphics::GetDevice();
+	
+	m_Camera.OnUpdate(deltaT, input);
+	m_Model->OnUpdate(deltaT);
+
+	m_SceneGPUData.time					= constantT;
+	m_SceneGPUData.view					= m_Camera.ViewMatrix;
+	m_SceneGPUData.proj					= m_Camera.ProjectionMatrix;
+
+	m_ModelGPUData.flip_uv_vertically	= m_Model->FlipUvVertically;
+	m_ModelGPUData.model				= m_Model->GetModelMatrix();
+	m_ModelGPUData.normal				= glm::mat4(glm::mat3(glm::transpose(glm::inverse(m_SceneGPUData.view * m_ModelGPUData.model))));
+
+	gfxDevice->UpdateBuffer(m_SceneDataBuffer, &m_SceneGPUData);
+	gfxDevice->UpdateBuffer(m_ModelDataBuffer, &m_ModelGPUData);
+}
+
+void Instancing::RenderScene(const uint32_t currentFrame, const VkCommandBuffer& commandBuffer) {
+	Graphics::GraphicsDevice* gfxDevice = Graphics::GetDevice();
+
+	gfxDevice->GetSwapChain().RenderTarget->Begin(commandBuffer);
+	gfxDevice->BindDescriptorSet(m_Set, commandBuffer, m_PSO.pipelineLayout, 0, 1);
+	
+	VkDeviceSize offsets[] = { sizeof(uint32_t) * m_Model->TotalIndices };
+
+	vkCmdBindVertexBuffers	(commandBuffer, 0, 1, &m_Model->DataBuffer.Handle, offsets);
+	vkCmdBindIndexBuffer	(commandBuffer, m_Model->DataBuffer.Handle, 0, VK_INDEX_TYPE_UINT32);
+	vkCmdBindPipeline		(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_PSO.pipeline);
+
+	for (const auto& mesh : m_Model->Meshes) {
+		m_PushConstant.materialIndex = mesh.MaterialIndex;
+
+		vkCmdPushConstants	(commandBuffer, m_PSO.pipelineLayout, VK_SHADER_STAGE_ALL_GRAPHICS, 0, sizeof(PushConstant), &m_PushConstant);
+		vkCmdDrawIndexed	(commandBuffer, static_cast<uint32_t>(mesh.Indices.size()), 1, static_cast<uint32_t>(mesh.IndexOffset), static_cast<int32_t>(mesh.VertexOffset), 0);
+	}
+}
+
+void Instancing::RenderUI() {
+	m_Camera.OnUIRender("Main Camera Settings");
+	m_Model->OnUIRender();
+}
+
+void Instancing::Resize(uint32_t width, uint32_t height) {
+	m_Width		= width;
+	m_Height	= height;
+}
+
+RUN_APPLICATION(Instancing);
---
+++ src/Samples/Instancing/fragment.glsl
@@ -0,0 +1,129 @@
+#version 450
+
+#extension GL_EXT_nonuniform_qualifier : enable
+#extension GL_ARB_shading_language_420pack : enable
+
+#define MAX_MATERIALS 50
+
+layout (std140, set = 0, binding = 0) uniform SceneGPUData {
+	float time;					// 4
+	int extra_1;				// 8
+	int extra_2;				// 12
+	int extra_3;				// 16
+	vec4 extra[7];				// 128
+	mat4 view;					// 192
+	mat4 proj;					// 256
+} sceneGPUData;
+
+layout (std140, set = 0, binding = 1) uniform ModelGPUData {
+	int flip_uv_vertically;		// 4
+	int extra_1;				// 8
+	int extra_2;				// 12
+	int extra_3;				// 16
+	vec4 extra[7];				// 128
+	mat4 model;					// 192
+	mat4 normal;				// 256
+} modelGPUData;
+
+layout (location = 0) in vec3 fragColor;
+layout (location = 1) in vec3 fragNormal;
+layout (location = 2) in vec3 fragPos;
+layout (location = 3) in vec2 fragTexCoord;
+
+layout (location = 0) out vec4 out_color;
+
+struct material_t {
+	vec4 ambient;		// ignore w
+	vec4 diffuse;		// ignore w
+	vec4 specular;		// ignore w
+	vec4 transmittance;	// ignore w
+	vec4 emission;		// ignore w
+	vec4 extra[6];
+
+	int pad2;
+	int illum;
+
+	int ambient_texture_index;
+	int diffuse_texture_index;
+	int specular_texture_index;
+	int bump_texture_index;
+	int roughness_texture_index;
+	int metallic_texture_index;
+	int normal_texture_index;
+	
+	int extra_scalar;
+
+	float shininess;
+	float ior;
+	float dissolve;
+	float roughness;
+	float metallic;
+	float sheen;
+	float clearcoat_thickness;
+	float clearcoat_roughness;
+	float anisotropy;
+	float anisotropy_rotation;
+	//float pad0;
+};
+
+layout (std140, set = 0, binding = 2) uniform material_uniform {
+	material_t materials[MAX_MATERIALS];
+};
+
+layout (set = 0, binding = 3) uniform sampler2D texSampler[];
+
+layout (push_constant) uniform constant {
+	int material_index;
+} constants;
+
+/*
+vec3 lightPos			= vec3(0.0, 2.0, 1.0);
+vec3 lightColor			= vec3(1.0);
+float lightIntensity	= 4.0;
+
+vec3 phongDiffuse(vec4 material_diffuse, vec4 material_normal) {
+	
+	vec3 lightDir = normalize(lightPos - fragPos);
+
+	float diff = max(dot(lightDir, vec3(material_normal)), 0.0);
+
+	vec3 diffuse = material_diffuse.rgb * diff * lightColor;
+
+	return diffuse * lightIntensity;
+}
+*/
+
+vec3 lightPos			= vec3(0.0, 2.0, 1.0);
+vec4 lightColor			= vec4(1.0, 1.0, 1.0, 1.0);
+float lightIntensity	= 1.0;
+
+vec4 phongDiffuse(vec4 material_diffuse, vec4 material_normal) {
+	vec3 lightDir	= normalize(lightPos - fragPos); 
+	float diff		= max(dot(lightDir, vec3(material_normal)), 0.0);
+
+	return material_diffuse * diff * (lightColor * lightIntensity);
+}
+
+void main() {
+	
+	material_t material		= materials[constants.material_index];
+
+	vec4 material_diffuse	= vec4(1.0);
+	vec4 material_normal	= vec4(1.0);
+	vec4 material_color		= vec4(0.0);
+
+	if (material.diffuse_texture_index == -1)
+		material_diffuse = material.diffuse;	
+	else
+		material_diffuse = texture(texSampler[material.diffuse_texture_index], fragTexCoord);	
+
+	if (material.normal_texture_index == -1)
+		material_normal = vec4(fragNormal, 1.0);
+	else
+		material_normal = texture(texSampler[material.normal_texture_index], fragTexCoord);
+
+	material_color += phongDiffuse(material_diffuse, material_normal);
+//	material_color += material_normal;
+
+	out_color = material_color;
+}
