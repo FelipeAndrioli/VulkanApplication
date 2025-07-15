25ee5f4acd8d319500739c396efc82383146e48a
Felipe Andrioli <felipeandrioli56@hotmail.com>
2025-07-15 05:28:56 -0300

    Added Point light settings/function to the shadow camera

--- src/Assets/ShadowCamera.cpp
+++ src/Assets/ShadowCamera.cpp
@@ -14,6 +14,35 @@ namespace Assets {
 		m_ShadowMatrix		= ProjectionMatrix * ViewMatrix;
 	}
 
+	void ShadowCamera::UpdateOmniDirectionalShadowMatrix(const glm::vec3& lightPosition) {
+
+		ProjectionMatrix = glm::perspective(glm::radians(PointLightFov), (float)m_Width / (float)m_Height, PointLightNear, PointLightFar);
+		
+		// Positive X
+		OmniViewMatrix[0] = glm::mat4(1.0f);
+		OmniViewMatrix[0] = glm::lookAt(lightPosition, lightPosition + glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+
+		// Negative X
+		OmniViewMatrix[1] = glm::mat4(1.0f);
+		OmniViewMatrix[1] = glm::lookAt(lightPosition, lightPosition + glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+
+		// Positive Y
+		OmniViewMatrix[2] = glm::mat4(1.0f);
+		OmniViewMatrix[2] = glm::lookAt(lightPosition, lightPosition + glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		
+		// Negative Y
+		OmniViewMatrix[3] = glm::mat4(1.0f);
+		OmniViewMatrix[3] = glm::lookAt(lightPosition, lightPosition + glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+
+		// Positive Z
+		OmniViewMatrix[4] = glm::mat4(1.0f);
+		OmniViewMatrix[4] = glm::lookAt(lightPosition, lightPosition + glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+		
+		// Negative Z
+		OmniViewMatrix[5] = glm::mat4(1.0f);
+		OmniViewMatrix[5] = glm::lookAt(lightPosition, lightPosition + glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+	}
+
 	void ShadowCamera::OnUIRender(const char* idCamera) {
 		if (ImGui::TreeNode(idCamera)) {
 			ImGui::DragFloat("Dir Light Frustum Size",	&m_DirLightFrustumSize, 0.02f, -40.0f, 40.0f);
@@ -24,13 +53,16 @@ namespace Assets {
 			ImGui::DragFloat("Spot Light Near", &m_SpotLightNear, 0.02f, 0.0f, 200.0f);
 			ImGui::DragFloat("Spot Light Far",	&m_SpotLightFar, 0.02f, 0.0f, 200.0f);
 
+			ImGui::DragFloat("Point Light Near", &PointLightNear, 0.02f, -1000.0f, 1000.0f);
+			ImGui::DragFloat("Point Light Far", &PointLightFar, 0.02f, -1000.0f, 1000.0f);
+			ImGui::DragFloat("Point Light FOV", &PointLightFov, 0.02f, -200.0f, 200.0f);
+
 			ImGui::DragFloat("Camera Position X",	&Position.x,	0.02f);
 			ImGui::DragFloat("Camera Position Y",	&Position.y,	0.02f);
 			ImGui::DragFloat("Camera Position Z",	&Position.z,	0.02f);
 			
 			ImGui::TreePop();
 		}
-
 	}
 
 	void ShadowCamera::Resize(uint32_t width, uint32_t height) {
@@ -48,5 +80,10 @@ namespace Assets {
 		m_DirLightNear			= near;
 		m_DirLightFar			= far;
 		m_DirLightFrustumSize	= frustumSize;
+	}
+
+	void ShadowCamera::SetPointLightSettings(float near, float far) {
+		PointLightNear	= near;
+		PointLightFar	= far;
 	}
 }
