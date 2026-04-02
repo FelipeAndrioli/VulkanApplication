59b7f64a014bf26ac30b3c02d7a3dead0527a3ca
Felipe Andrioli <felipeandrioli56.fa@gmail.com>
2026-04-02 05:50:37 -0300

    Copying light composition result to swap chain render target even if post effects are disabled

--- src/Samples/AmbientOcclusion/post_process_fragment.glsl
+++ src/Samples/AmbientOcclusion/post_process_fragment.glsl
@@ -8,8 +8,8 @@ layout (location = 0) out vec4 frag_color;
 
 layout (std140, set = 0, binding = 0) uniform PostEffectsGPUData {
     vec4 extra[15];
+    int flags;
     float gamma;
-    float extra1;
     float extra2;
     float extra3;
 } post_effects_gpu_data;
@@ -17,8 +17,15 @@ layout (std140, set = 0, binding = 0) uniform PostEffectsGPUData {
 layout (set = 0, binding = 1) uniform sampler2D geometry_pass_result;
 
 void main() {
+
+    bool post_effects_enabled = bool(post_effects_gpu_data.flags & 1);
+    bool gamma_correction_enabled = bool(post_effects_gpu_data.flags & (1 << 1));
+
     vec3 color = texture(geometry_pass_result, uv).rgb;
-    color = pow(color, vec3(1.0 / post_effects_gpu_data.gamma));
+
+    if (post_effects_enabled && gamma_correction_enabled) {
+        color = pow(color, vec3(1.0 / post_effects_gpu_data.gamma));
+    }
 
     frag_color = vec4(color, 1.0); 
 }
